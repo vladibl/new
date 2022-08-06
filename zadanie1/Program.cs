f100878641b62f702667249627d60c957c94ffdf
Vlad <vladibladi@yandex.ru>
2022-08-06 10:21:48 +0300

    zadanie2

--- zadanie1/Program.cs
+++ zadanie1/Program.cs
@@ -5,10 +5,11 @@ b = Convert.ToInt32(Console.ReadLine());
 if(a>b)
 {
     Console.WriteLine("{0} наибольшее число",a);
+    Console.WriteLine("{0} наименьшее число",b);
 }
 else
 {
         Console.WriteLine("{0} наибольшее число",b);
-
+        Console.WriteLine("{0} наименьшее число",a);
 }
 Console.ReadLine();
