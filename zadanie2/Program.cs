759f02d30d378a130ccb0a258f921ae8c1b39315
Vlad <vladibladi@yandex.ru>
2022-08-06 12:16:02 +0300

    zadanie2

---
+++ zadanie2/Program.cs
@@ -0,0 +1,11 @@
+﻿int a,b,c;
+int max;
+Console.WriteLine("введите числа: ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+max=a;
+if (b>max)max=b;
+if (c>max)max=c;
+
+Console.WriteLine("{0} наибольшее число",max);
---
+++ zadanie3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("введите число: ");
+if (int.Parse(Console.ReadLine()) % 2 == 0)
+                Console.WriteLine("Это чётное число");
+            else Console.WriteLine("Это нечётное число");
+            Console.ReadKey();checked
