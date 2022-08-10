547cd0516332130b90636561e47fa7a015ff8cdc
Vlad <vladibladi@yandex.ru>
2022-08-10 12:35:49 +0300

    zadanie10

---
+++ zadanie10/Program.cs
@@ -0,0 +1,8 @@
+﻿int a;
+Console.WriteLine("введите трехзначное число: ");
+a = Convert.ToInt32(Console.ReadLine());
+int num1 = a / 10;
+int num2 = num1 % 10;
+
+int result = num2;
+Console.WriteLine($"число такое {result} из {a}");
