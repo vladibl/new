8997e61222045d33e8f2ff943e5356b673816d30
Vlad <vladibladi@yandex.ru>
2022-08-10 22:09:28 +0300

    zadanie15

---
+++ zadanie15/Program.cs
@@ -0,0 +1,26 @@
+﻿int a;
+int t;
+int n;
+a=1;
+t=6;
+n=7;
+Console.WriteLine("введите  число 1-7: ");
+a = Convert.ToInt32(Console.ReadLine());
+if (a < 6)
+{
+Console.WriteLine($"число {a} будний день");
+}
+else if (a == 6)
+{
+    Console.WriteLine($"число {t} выходной день");
+}
+else if (a == 7)
+{
+    Console.WriteLine($"число {n} выходной день");
+}
+
+
+
+
+
+
