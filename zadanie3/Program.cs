0873fa648daf80fb52c3e925741f39795df3c3fc
Vlad <vladibladi@yandex.ru>
2022-08-10 12:53:30 +0300

    zadanie

--- zadanie3/Program.cs
+++ zadanie3/Program.cs
@@ -2,4 +2,4 @@
 if (int.Parse(Console.ReadLine()) % 2 == 0)
                 Console.WriteLine("Это чётное число");
             else Console.WriteLine("Это нечётное число");
-            Console.ReadKey();
+            Console.WriteLine();
---
+++ zadanie4/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int number = new Random() .Next(100,1000);
+int num1 = number % 10;
+int num2 = number / 100;
+int result = num2 * 10 + num1;
+Console.WriteLine($"число такое {result} из {number}");
+
+
+
