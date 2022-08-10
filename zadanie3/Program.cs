06b0cfd31333f2f69a173caca8d26c73903db576
vladibl <109811316+vladibl@users.noreply.github.com>
2022-08-10 13:04:14 +0300

    Delete zadanie3 directory

--- zadanie3/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿Console.WriteLine("введите число: ");
-if (int.Parse(Console.ReadLine()) % 2 == 0)
-                Console.WriteLine("Это чётное число");
-            else Console.WriteLine("Это нечётное число");
-            Console.WriteLine();
