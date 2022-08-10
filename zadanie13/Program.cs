59162cb80684fd0a29518bbb3630ec82dc8e1aa2
Vlad <vladibladi@yandex.ru>
2022-08-10 16:15:35 +0300

    zadanie13

---
+++ zadanie13/Program.cs
@@ -0,0 +1,19 @@
+﻿int a;
+Console.WriteLine("введите трехзначное число: ");
+a = Convert.ToInt32(Console.ReadLine());
+if(a > 99)
+{
+    Console.WriteLine((a / 100) % 10 );
+}
+else
+{
+    Console.WriteLine("третьего числа нет");
+
+}
+
+
+
+
+
+
+
