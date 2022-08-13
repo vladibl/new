0eb3f099bd768d49755063b580b79a2d1c77dff8
Vlad <vladibladi@yandex.ru>
2022-08-13 12:24:33 +0300

    zadanie19

---
+++ zadanie19/Program.cs
@@ -0,0 +1,23 @@
+﻿int a;
+Console.WriteLine("введите пятизначное  число: ");
+a = Convert.ToInt32(Console.ReadLine());
+int bum = a;
+int n2 = 0;
+while ( a > 0 )
+{
+  n2 = ( n2 * 10 ) + ( a % 10);
+a = ( a / 10 );
+}
+
+Console.WriteLine($"{n2}" );
+
+if (bum == n2)
+{
+    Console.WriteLine("число палиндром");
+}
+else
+{
+    Console.WriteLine("число не палиндром");
+
+}
+
