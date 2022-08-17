9fc8aae71eac1eab2045e2437067d86104b146dc
Vlad <vladibladi@yandex.ru>
2022-08-17 10:15:05 +0300

    zadanie27

---
+++ zadanie27/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int DigitCount(int a)
+{
+    int result = 0;
+
+    result = a % 10;
+    a = a / 10;
+    if (a > 0)
+    {
+        result = result + DigitCount(a);
+    }
+    return result;
+}
+Console.WriteLine(DigitCount(num));
+
+
+
+
+
+
+
+
+
