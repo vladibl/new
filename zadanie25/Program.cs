8ef934ca34b191104dc979e2261d65c8db273df0
Vlad <vladibladi@yandex.ru>
2022-08-17 12:01:12 +0300

    zadanie25

---
+++ zadanie25/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("введите два числа: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int DigitCount(int num, int num2)
+{
+    int result = 1;
+    for (int i = 1; i <= num2; i++)
+    {
+        result = result * num;
+    
+
+    }
+    return result;
+}
+DigitCount (num,num2);
+
+Console.WriteLine(DigitCount(num,num2));
