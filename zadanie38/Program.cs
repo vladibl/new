4e7d769a660436d0a004600e5f6c7e45cd67d9c5
Vlad <vladibladi@yandex.ru>
2022-08-20 14:52:47 +0300

    zadanie38

---
+++ zadanie38/Program.cs
@@ -0,0 +1,41 @@
+﻿
+int findMin(int[] arr)
+{
+
+
+    int min = int.MaxValue;
+    foreach (var i in arr)
+    {
+        if (i < min)
+        {
+            min = i;
+        }
+    }
+    return min;
+}
+
+int findMax(int[] arr)
+{
+
+
+    int max = int.MinValue;
+    foreach (var i in arr)
+    {
+        if (i > max)
+        {
+            max = i;
+        }
+    }
+    return max;
+}
+
+
+
+int[] arr = { 8, 3, 5, 1, 2 };
+
+
+
+Console.WriteLine(findMax(arr) - findMin(arr));
+
+
+
