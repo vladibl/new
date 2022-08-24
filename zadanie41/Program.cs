32c1c8caee0dcaebfdafd24aa57cec0f0eb33a0d
Vlad <vladibladi@yandex.ru>
2022-08-24 11:41:42 +0300

    zadanie41

---
+++ zadanie41/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите количество цифр : ");
+int elementCount = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[elementCount];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите число {i} ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+
+}
+
+int Comparison(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count += 1;
+    }
+    return count;
+}
+
+
+
+Console.WriteLine($"Введено чисел больше 0: {Comparison(array)} ");
