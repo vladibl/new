c1431ca3285141cb8c5ec09e2bd5983b5a3c4307
Vlad <vladibladi@yandex.ru>
2022-09-05 10:20:07 +0300

    zadanie66

---
+++ zadanie66/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int s = 0;
+
+int Addition(int m, int n)
+{
+    if (m > n)
+    {
+        for (int i = n; i <= m; i++)
+        {
+            s += i;
+        }
+    }
+    if (m < n)
+    {
+        for (int i = m; i <= n; i++)
+        {
+            s += i;
+        }
+       
+    }
+    return(s);
+}
+Addition(m,n);
+Console.WriteLine(s);
+
