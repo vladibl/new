d1ef8111996c6d27d6e313e7d09e98ef817fdfb2
Vlad <vladibladi@yandex.ru>
2022-09-15 13:35:31 +0300

    2

--- Проверочная работа/Program.cs
+++ Проверочная работа/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = new string[7] { "34", "1", "hello", "world", "result","war","no" };
+string[] array2 = new string[array1.Length];
+void Arraysec(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void Printarray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Arraysec(array1, array2);
+Printarray(array2);
