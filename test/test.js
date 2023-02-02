139d3fd34122895333eccbae4556a285085f21ef
vladibl <109811316+vladibl@users.noreply.github.com>
2023-02-02 14:21:34 +0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,46 +1,27 @@
 import { Selector } from 'testcafe';
-
-
+    
 fixture `Example page`
-    .page `https://todomvc.com/examples/typescript-backbone/`;
+        .page `https://todomvc.com/examples/typescript-backbone/`;
+//ввели запрос
+        test('Emulate user actions and perform a verification', async t => {
+        const inputNewTodo = ('body > section > header > div > input')
+        await t.typeText(inputNewTodo,'Peter Parker\r\n')
+//отметили галочкой все запросы
+        const addedTodoElement =('body > section > section > label') 
+        await t.click (addedTodoElement)
+       
+//убрали галочку с отмеченного запросы
+       const addedTodoCheckboxAC =('body > section > section > ul > li > div > input')
+        await t.click(addedTodoCheckboxAC)
+//двойеное нажатие на запрос
+       const addedTodoEditInput=('body > section > section > ul > li:nth-child(1) > div > label')
+       await t.click(addedTodoEditInput)
 
-    test('Emulate user actions and perform a verification', async t => {
-        await t.setNativeDialogHandler(() => true);
-        const inputNewTodo = await querySelector('new-todo');
-        await t.typeText(inputNewTodo, 'New TODO element\r\n');
-    
-        const addedTodoElement = await querySelectorCondition(
-            'section.main label',
-            (elm) => (elm.innerText === 'New TODO element')
-        );
-    
-        await t.doubleClick(addedTodoElement);
-    
-        const addedTodoEditInput = await querySelectorCondition(
-            'section.main input[type=text]',
-            (elm) => (elm.value === 'New TODO element')
-        );
-    
-        await t.typeText(addedTodoEditInput, ' changed\r\n');
-    
-        const addedTodoCheckboxAC = await querySelectorCondition(
-            'section.main input[type=checkbox]:not([checked])',
-            (elm) => (elm.nextSibling.innerText === 'New TODO element changed')
-        );
-    
-        await t.click(addedTodoCheckboxAC);
-    
-        const addedTodoCheckboxBC = await querySelectorCondition(
-            'section.main input[type=checkbox]',
-            (elm) => (elm.nextSibling.innerText === 'New TODO element changed')
-        );
-    
-        await t.click(addedTodoCheckboxBC);
-    
-        const addedTodoDelBtn = await querySelectorCondition(
-            'button.destroy',
-            (elm) => (elm.previousSibling.innerText === 'New TODO element changed')
-        );
-    
+      //удаляем запрос
+      const addedTodoDelBtn = (
+        'button.destroy')
         await t.click(addedTodoDelBtn);
-    });
+        (elm) => (elm.previousSibling.innerText === 'New TODO element changed')
+        
+         
+});
